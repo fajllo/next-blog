5262d48b0500894009923e83247b9f66e71a2092
fsdr <filipsordyl8@gmail.com>
2022-01-25 18:41:56 +0100

    added navbar

---
+++ components/Navbar.js
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Navbar() {
+  const user = true;
+  const username = true;
+  return (
+    <nav className="Navbar w-screen h-16 bg-neutral-200 flex items-center text-2xl ">
+      <ul className="flex justify-between w-full mx-4 md:mx-12">
+        <li>
+          {" "}
+          <Link href="/">
+            <button className="text-white bg-gray-800 py-2 px-4 rounded-lg">
+              Fsdr blog
+            </button>
+          </Link>
+        </li>
+        {/* {user is signed in and has user name} */}
+        {username && (
+          <div>
+            <li>
+              <Link href="/admin">
+                <button className="text-white bg-emerald-500 py-2 px-4 rounded-lg">
+                  write post
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                {/* pierwsze jest dobre tylko trzeba to skonfigurowac jak ju będą jakieś zdjęcia  */}
+                {/* <Image src={{user?.photoURL}}></Image>  */}
+                {/* <Image
+                  src={
+                    "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2360&q=80"
+                  }
+                ></Image> */}
+                <img src={user?.photoURL} />
+              </Link>
+            </li>
+          </div>
+        )}
+        {/* {user is NOT signed or has not created user name} */}
+        {!username && (
+          <>
+            <li>
+              <Link href="/enter">
+                <button className="text-white bg-violet-500 py-2 px-4 rounded-lg">
+                  sign in
+                </button>
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+}
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,9 +1,8 @@
 import Loader from "../../components/Loader";
+
 export default function AdminPostPage({}) {
   return (
     <main>
-      {" "}
-      <h1>admin page </h1>
       <Loader show={true} />
     </main>
   );

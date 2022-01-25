279d5b2e0c8776c882092b48d77fcc39ede41fde
fsdr <filipsordyl8@gmail.com>
2022-01-25 18:50:12 +0100

    added react-hot toast

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import toast from "react-hot-toast";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div>
+        <button onClick={() => toast.success("hello toast!")}>click me</button>
+      </div>
+    </>
+  );
 }

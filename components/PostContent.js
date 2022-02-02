975bf6a7ecb90708131c10ffcc55b23a29a52043
fsdr <filipsordyl8@gmail.com>
2022-02-02 17:54:44 +0100

    added react ISR to username/slug page

---
+++ components/PostContent.js
@@ -0,0 +1,3 @@
+export default function PostContent() {
+  return <div></div>;
+}
--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,8 +1,40 @@
-export default function PostPage({}) {
-    return (
-      <main>
-        {" "}
-        <h1>admin page </h1>
-      </main>
-    );
+import { getUserWithUsername, postToJSON, firestore } from "../../lib/firebase";
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+
+  const userDoc = await getUserWithUsername(username);
+  // JSON serializable data
+  let post = null;
+  let path = null;
+
+  if (userDoc) {
+    const postRef = userDoc.ref.collection("posts").doc(slug);
+    post = postToJSON(await postRef.get());
+
+    path = postRef.path;
   }
+  return {
+    props: { post, path },
+    revalidate: 5000,
+  };
+}
+export async function getStaticPaths() {
+  const snapshot = await firestore.collection("posts").get();
+  const paths = snapshot.docs.map(doc => {
+    const { slug, username } = doc.data();
+    return { params: { username, slug } };
+  });
+  return {
+    paths,
+    fallback: "blocking",
+  };
+}
+
+export default function PostPage({}) {
+  return (
+    <main>
+      {" "}
+      <h1>admin page </h1>
+    </main>
+  );
+}
